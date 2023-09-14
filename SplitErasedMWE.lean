bf01ee19e6ecaed6a2c15a64f9238c36fbb3800e
Alex Keizer <alex@keizer.dev>
2023-09-14 17:06:01 +0100

    Fix MWE with new toolchain

--- SplitErasedMWE.lean
+++ SplitErasedMWE.lean
@@ -1,11 +1,46 @@
-import Mathlib.Data.Erased
+def Erased (α : Sort u) : Sort max 1 u :=
+  Σ's : α → Prop, ∃ a, (fun b => a = b) = s
+
+namespace Erased
+
+def mk {α} (a : α) : Erased α :=
+  ⟨fun b => a = b, a, rfl⟩
+
+/-- Extracts the erased value, noncomputably. -/
+noncomputable def out {α} : Erased α → α
+  | ⟨_, h⟩ => Classical.choose h
+
+theorem mk_out {α} : ∀ a : Erased α, mk (out a) = a
+  | ⟨s, h⟩ => by simp [mk]; congr; exact Classical.choose_spec h
+
+@[simp]
+theorem out_mk {α} (a : α) : (mk a).out = a := by
+  let h := (mk a).2; show Classical.choose h = a
+  have := Classical.choose_spec h
+  exact cast (congrFun this a).symm rfl
+
+@[injection]
+theorem out_inj {α} (a b : Erased α) (h : a.out = b.out) : a = b :=
+  Eq.mp (congr (congrArg Eq (mk_out a)) (mk_out b)) (congrArg mk h)
+
+@[injection]
+theorem mk_inj (h : Erased.mk a = Erased.mk b) : a = b := by
+  injection h with fst_eq snd_eq
+  have (b') : (a = b') = (b = b') := by
+    show (fun b => a = b) b' = (fun b_1 => b = b_1) b'
+    rw [fst_eq]
+  rw [this]
+
+end Erased
 
 inductive Ty
   | nat
+  | bool
 
 def Ctxt : Type :=
   Erased <| List Ty
 
+@[match_pattern]
 noncomputable def Ctxt.snoc (Γ : Ctxt) (t : Ty) : Ctxt :=
   Erased.mk <| t :: Γ.out
 
@@ -14,68 +49,41 @@ def Ctxt.Var (Γ : Ctxt) (t : Ty) : Type :=
 
 inductive CtxtProp : Ctxt → Type
   | nil : CtxtProp (Erased.mk [])
-  | snoc : CtxtProp Δ → CtxtProp (Δ.snoc t)
+  | snoc : CtxtProp Δ → (t : Ty) → CtxtProp (Δ.snoc t)
 
 
-noncomputable def matchVar {t : Ty} {Δ : Ctxt} : CtxtProp Δ → Δ.Var t → Option Bool
-  | .snoc d, ⟨w+1, h⟩ => -- w† = Var.toSnoc w
+noncomputable def matchVar {Δ : Ctxt} {t} : CtxtProp Δ → Δ.Var t  → Option Bool
+  | .snoc d _, ⟨w+1, h⟩ => -- w† = Var.toSnoc w
       let w : Ctxt.Var _ t := ⟨w, by simp_all[List.get?, Ctxt.snoc]⟩
       matchVar d w
   -- Neither spelling out all cases, nor having a single fallback case work
-  | .snoc _, ⟨0, _⟩ | .nil, ⟨0, _⟩ | .nil, ⟨_+1, h⟩ => none
+  | .nil, _ | .snoc .., ⟨0, _⟩ => none
   -- | _, _ => none
 
-set_option trace.Meta.Match.matchEqs true
+@[injection]
+theorem Erased.mk_snoc (h : Erased.mk [] = Ctxt.snoc Γ t) : [] = t :: Γ.out :=
+  Erased.mk_inj h
+
+@[injection]
+theorem Erased.snoc_mk (h : Ctxt.snoc Γ t = Erased.mk []) : t :: Γ.out = [] :=
+  Erased.mk_inj h
+
+@[injection]
+theorem Ctxt.snoc_inj (h : Ctxt.snoc Γ₁ t₁ = Ctxt.snoc Γ₂ t₂) : t₁ = t₂ ∧ Γ₁ = Γ₂ := by
+  have := Erased.mk_inj h
+  injection this with t_eq Γ_eq
+  exact ⟨t_eq, Erased.out_inj _ _ Γ_eq⟩
+
+-- set_option trace.Meta.Tactic.injection true
+-- set_option trace.Meta.Match.matchEqs true
 example {Δ : Ctxt} (d : CtxtProp Δ) {w : Δ.Var t} :
   matchVar d w = none
     := by
-        unfold matchVar /- throws error:
-          failed to generate splitter for match auxiliary declaration 'matchVar.match_1', unsolved subgoal:
-            case x
-            t: Ty
-            motive: (Δ : Ctxt) → CtxtProp Δ → Ctxt.Var Δ t → Sort u_1
-            h_1: (Γ : Ctxt) →
-              (t_2 : Ty) →
-                (d : CtxtProp Γ) →
-                  (w : ℕ) →
-                    (h : List.get? (Erased.out (Ctxt.snoc Γ t_2)) (w + 1) = some t) →
-                      motive (Ctxt.snoc Γ t_2) (CtxtProp.snoc d) { val := Nat.succ w, property := h }
-            h_2: (Γ : Ctxt) →
-              (t_2 : Ty) →
-                (a : CtxtProp Γ) →
-                  (property : List.get? (Erased.out (Ctxt.snoc Γ t_2)) 0 = some t) →
-                    (∀ (Γ_1 : Ctxt) (t_1 : Ty) (d : CtxtProp Γ_1) (w : ℕ)
-                        (h : List.get? (Erased.out (Ctxt.snoc Γ_1 t_1)) (w + 1) = some t),
-                        ((fun b => t_2 :: Erased.out Γ = b) = fun b => t_1 :: Erased.out Γ_1 = b) →
-                          HEq (_ : ∃ a, (fun b => a = b) = fun b => t_2 :: Erased.out Γ = b)
-                              (_ : ∃ a, (fun b => a = b) = fun b => t_1 :: Erased.out Γ_1 = b) →
-                            HEq (CtxtProp.snoc a) (CtxtProp.snoc d) →
-                              HEq { val := 0, property := property } { val := Nat.succ w, property := h } → False) →
-                      motive (Ctxt.snoc Γ t_2) (CtxtProp.snoc a) { val := 0, property := property }
-            h_3: (property : List.get? (Erased.out (Erased.mk [])) 0 = some t) →
-              motive (Erased.mk []) CtxtProp.nil { val := 0, property := property }
-            h_4: (n : ℕ) →
-              (h : List.get? (Erased.out (Erased.mk [])) (n + 1) = some t) →
-                motive (Erased.mk []) CtxtProp.nil { val := Nat.succ n, property := h }
-            Δ✝: Ctxt
-            t✝: Ty
-            a✝: CtxtProp Δ✝
-            x✝²: Ctxt.Var (Ctxt.snoc Δ✝ t✝) t
-            val✝: ℕ
-            property✝¹: List.get? (Erased.out (Ctxt.snoc Δ✝ t✝)) val✝ = some t
-            property✝: List.get? (Erased.out (Ctxt.snoc Δ✝ t✝)) Nat.zero = some t
-            Γ: Ctxt
-            t_1: Ty
-            d: CtxtProp Γ
-            w: ℕ
-            h: List.get? (Erased.out (Ctxt.snoc Γ t_1)) (w + 1) = some t
-            fst_eq: (fun b => t✝ :: Erased.out Δ✝ = b) = fun b => t_1 :: Erased.out Γ = b
-            snd_eq: HEq (_ : ∃ a, (fun b => a = b) = fun b => t✝ :: Erased.out Δ✝ = b)
-              (_ : ∃ a, (fun b => a = b) = fun b => t_1 :: Erased.out Γ = b)
-            x_1: HEq (CtxtProp.snoc a✝) (CtxtProp.snoc d)
-            x_2: HEq { val := 0, property := property✝ } { val := Nat.succ w, property := h }
-            x✝¹: CtxtProp (Ctxt.snoc Δ✝ t✝)
-            x✝: Ctxt.Var (Ctxt.snoc Δ✝ t✝) t
-            ⊢ False
-        -/
-        sorry
+        cases w; next w h =>
+        induction d generalizing w <;> cases w
+        . rfl
+        . rfl
+        . rfl
+        next ih _ => -- simp [matchVar]
+          simp [matchVar]
+          apply ih
